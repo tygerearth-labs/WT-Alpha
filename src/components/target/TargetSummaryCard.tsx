23ea6c1c3e7b8558e5b5f021871181684cc3d13b
tygerearth-labs <tygerearth@gmail.com>
2026-02-03 19:49:39 +0700

    Update TargetSummaryCard.tsx

--- src/components/target/TargetSummaryCard.tsx
+++ src/components/target/TargetSummaryCard.tsx
@@ -112,7 +112,7 @@ export function TargetSummaryCard({ savingsTargets }: TargetSummaryCardProps) {
       </CardHeader>
       <CardContent>
         {/* 6 COLUMN GRID LAYOUT */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           
           {/* COLUMN 1: Brutal Insight */}
           <div className="lg:col-span-1 bg-background/50 backdrop-blur-sm rounded-lg p-4 border border-border/50">
