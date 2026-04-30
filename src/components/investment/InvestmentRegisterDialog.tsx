e4fda0b842217965a0d5fd6c4578bc2d4a8e898c
blackbearlabs888 <tygerearth@gmail.com>
2026-04-30 11:23:35 +0700

    Update InvestmentRegisterDialog.tsx

--- src/components/investment/InvestmentRegisterDialog.tsx
+++ src/components/investment/InvestmentRegisterDialog.tsx
@@ -14,6 +14,7 @@ import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { useTranslation } from '@/hooks/useTranslation';
 import { useBusinessStore } from '@/store/useBusinessStore';
+import { cn } from '@/lib/utils';
 import { toast } from 'sonner';
 import {
   Loader2,
